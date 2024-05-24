e71adbc09632d58385c546db4a0a549c07119eb1
Anaidys Jimenez <u.anaidys@gmail.com>
2024-05-23 23:45:48 -0400

    created files for about, admin, create, and home

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -2,7 +2,83 @@ import React from 'react';
 
 const Admin = () => {
     return (
-      <div>Admin</div>
+      <div className="Admin">
+        <h1>ADMIN PAGE</h1>
+        <button className="new-button" type="submit">ADD A NEW COMIC</button>
+     
+<table>
+    <thead>  
+        <tr>
+            <th>COMIC TITLE</th>
+            <th>EDIT</th>
+            <th>DELETE</th>
+        </tr>
+    </thead>
+
+<tbody>
+<tr>
+    <td>Batman: The Dark Knight Returns</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>Black Panther: A Nation Under Our Feet Book 1 </td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>Fun Home: A Family Tragicomic</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>Hunter X Hunter Vol. 1</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>Lumberjanes Vol. 1</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>March: Book One</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>One Piece, Vol. 1: Romance Dawn</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>Parable of the Sower</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>Queer: A Graphic History</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>The Walking Dead, Vol. 1: Days Gone Bye</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>Wake: The Hidden History of Women-Led Slave Revolts</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+<tr>
+    <td>Watchmen</td>
+    <td><button className="edit-button" type="submit">EDIT</button></td>
+    <td><button className="delete-button" type="submit">DELETE</button></td>
+</tr>
+</tbody>
+</table>
+      </div>
     )
   }
 
--- src/components/Create.js
+++ src/components/Create.js
@@ -2,7 +2,72 @@ import React from 'react';
 
 const Create = () => {
     return (
-      <div>Create</div>
+      <div className="Create">
+        <h1>CREATE NEW COMIC</h1>
+<form action="#" className="new-comic-form">
+
+<div className="comic-forms">
+    <div>
+        <label htmlFor="title">Title:</label>
+        <input type="text" name="title"  id="title" placeholder="Title"/>
+    </div>
+    
+
+    <div>
+        <label htmlFor="author">Author:</label>
+        <input type="text" name="author"  id="author" placeholder="Author"/>
+    </div>
+    
+
+    <div>
+<label htmlFor="publisher-select">Publisher:</label>
+<select name="publishers" id="publisher-select">
+    <option value="select">Select</option>
+    <option value="boom">BOOM! Box</option>
+    <option value="dc">DC Comics</option>
+    <option value="abrams">Henry N. Abrams</option>
+    <option value="icon">Icon Books</option>
+    <option value="image">Image Comics</option>
+    <option value="marvel">Marvel</option>
+    <option value="s and s">Simon & Schuster</option>
+    <option value="tsp">Top Shelf Productions</option>
+    <option value="viz">Viz Media LLC</option>
+</select>
+</div>
+
+    <div>
+        <label htmlFor="genre">Genre:</label>
+        <input type="text" name="genre"  id="genre" placeholder="Genre"/>
+    </div>
+    
+
+    <div>
+        <label htmlFor="pages">Number of Pages:</label>
+        <input type="text" name="pages"  id="pages" placeholder="Number of Pages"/>
+    </div>
+  
+
+    <div>
+        <label htmlFor="rating">Rating</label>
+        <input type="text" name="rating"  id="rating" placeholder="number (0-5)"/>
+    </div>
+
+
+    <div>
+        <label htmlFor="synopsis">Synopsis:</label>
+        <input type="text" name="synopsis"  id="synopsis" placeholder="Synopsis"/>
+
+        
+    </div>
+
+</div> 
+
+    <button className="submit-button" type="submit">Submit</button> 
+
+</form>
+
+
+      </div>
     )
   }
 
